a031b0059cb11cb07a2a331161ae621b9f6ede56
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2023-08-28 12:55:24 +0200

    Added caching and fixed test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(home: MyApp()));
 
     // Verify that platform version is retrieved.
     expect(
