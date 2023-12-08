1fa5a0475c2c51f2f7eab7fe74aa02dfac435c15
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2023-12-08 10:52:46 +0100

    Deleted dead code

--- lib/src/utils/sdk_version_number_provider.dart
+++ lib/src/utils/sdk_version_number_provider.dart
@@ -5,17 +5,4 @@ class SdkVersionNumberProvider {
   String getSdkVersionNumber() {
     return Constants.versionNumber;
   }
-
-// Future<String> getSdkVersionNumberFromPubspec() async {
-//   // requires pubspec provided via asset
-//   // assets:
-//   //  - pubspec.yaml
-//
-//   final pubspecContent =
-//       await rootBundle.loadString("packages/adyen_checkout/pubspec.yaml");
-//   final RegExp regex = RegExp('version:s*(.*?)s*\\n');
-//   final match = regex.firstMatch(pubspecContent);
-//   final versionNumber = match?.group(1) ?? "";
-//   return versionNumber;
-// }
 }
