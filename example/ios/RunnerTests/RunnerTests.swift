22e1ffcd50e43055d4ddb1db98e9704dde9021ce
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2024-01-10 14:29:25 +0100

    Added example unit test for iOS layer

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import XCTest
+import Adyen
 
 @testable import adyen_checkout
 
@@ -9,9 +10,33 @@ import XCTest
 // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
 
 class RunnerTests: XCTestCase {
-    func testGetPlatformVersion() {
-        let result = true
-        XCTAssert(result, "Result is false")
-
+    private let TEST_CLIENT_KEY = "test_qwertyuiopasdfghjklzxcvbnmqwerty"
+    
+    func testWhenDropInConfigurationDtoIsProvidedThenMapItToNativeSdkModel() {
+        do {
+            let dropInConfigurationDTO = DropInConfigurationDTO(
+                environment: Environment.test,
+                clientKey: TEST_CLIENT_KEY,
+                countryCode: "US",
+                amount: AmountDTO(currency: "USD", value: 1600),
+                shopperLocale: "en-US",
+                analyticsOptionsDTO: AnalyticsOptionsDTO(enabled: false, version: "0.0.1"),
+                showPreselectedStoredPaymentMethod: false,
+                skipListWhenSinglePaymentMethod: false,
+                isRemoveStoredPaymentMethodEnabled: false)
+            
+            let adyenContext = try dropInConfigurationDTO.createAdyenContext()
+            let dropInConfiguration = try ConfigurationMapper().createDropInConfiguration(dropInConfigurationDTO: dropInConfigurationDTO)
+            
+            XCTAssertEqual(adyenContext.apiContext.environment.baseURL, Adyen.Environment.test.baseURL)
+            XCTAssertEqual(adyenContext.apiContext.clientKey, TEST_CLIENT_KEY)
+            XCTAssertEqual(adyenContext.payment?.countryCode, "US")
+            XCTAssertEqual(adyenContext.payment?.amount.currencyCode, "USD")
+            XCTAssertEqual(adyenContext.payment?.amount.value, 1600)
+            XCTAssertEqual(dropInConfiguration.allowPreselectedPaymentView, false)
+            XCTAssertEqual(dropInConfiguration.allowsSkippingPaymentList, false)
+        } catch {
+            XCTAssert(false, "Failed")
+        }
     }
 }
