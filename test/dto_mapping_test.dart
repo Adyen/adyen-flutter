fe0b9cafd3fac82bb08871bd5d7e870fa071121f
Robert Schulze Dieckhoff <13377878+Robert-SD@users.noreply.github.com>
2024-06-24 10:01:13 +0200

    Added unit test for card and GooglePay configurations (#193)
    * Added unit test for card and GooglePay configurations
    
    * Added test for apple pay configuration parsing
    
    * Removed generated code from lcov report
    
    * Cash app pay configuration now uses named parameters
    
    * Made stored payment method configuration values optional and added unit test
    
    * added parameter name to callback
    
    * Replaced fixed start and end date with dynamic value

--- test/dto_mapping_test.dart
+++ test/dto_mapping_test.dart
@@ -12,13 +12,18 @@ void main() {
     const currency = "USD";
     const amountValue = 1286;
     const shopperLocal = "en-US";
-    final dropInConfiguration = DropInConfiguration(
-      environment: Environment.test,
-      clientKey: demoClientKey,
-      countryCode: countryCode,
-      amount: Amount(value: amountValue, currency: currency),
-      shopperLocale: shopperLocal,
+    final storedPaymentMethodConfiguration = StoredPaymentMethodConfiguration(
+      showPreselectedStoredPaymentMethod: true,
+      isRemoveStoredPaymentMethodEnabled: true,
+      deleteStoredPaymentMethodCallback: (String input) => Future.value(true),
     );
+    final dropInConfiguration = DropInConfiguration(
+        environment: Environment.test,
+        clientKey: demoClientKey,
+        countryCode: countryCode,
+        amount: Amount(value: amountValue, currency: currency),
+        shopperLocale: shopperLocal,
+        storedPaymentMethodConfiguration: storedPaymentMethodConfiguration);
 
     final dropInConfigurationDto = dropInConfiguration.toDTO("0.0.1");
 
@@ -34,7 +39,215 @@ void main() {
     expect(dropInConfigurationDto.googlePayConfigurationDTO, null);
     expect(dropInConfigurationDto.cashAppPayConfigurationDTO, null);
     expect(dropInConfigurationDto.analyticsOptionsDTO.enabled, true);
-    expect(dropInConfigurationDto.isRemoveStoredPaymentMethodEnabled, false);
     expect(dropInConfigurationDto.skipListWhenSinglePaymentMethod, false);
+    expect(dropInConfigurationDto.showPreselectedStoredPaymentMethod, true);
+    expect(dropInConfigurationDto.isRemoveStoredPaymentMethodEnabled, true);
+  });
+
+  test(
+      "when using card configuration, then should parse to CardConfigurationDTO",
+      () {
+    const cardConfiguration = CardConfiguration(
+      holderNameRequired: true,
+      addressMode: AddressMode.full,
+      showStorePaymentField: true,
+      showCvcForStoredCard: true,
+      showCvc: true,
+      kcpFieldVisibility: FieldVisibility.hide,
+      socialSecurityNumberFieldVisibility: FieldVisibility.show,
+      supportedCardTypes: ["amex"],
+    );
+
+    final cardConfigurationDTO = cardConfiguration.toDTO();
+
+    expect(cardConfigurationDTO.holderNameRequired, true);
+    expect(cardConfigurationDTO.addressMode, AddressMode.full);
+    expect(cardConfigurationDTO.showStorePaymentField, true);
+    expect(cardConfigurationDTO.showCvcForStoredCard, true);
+    expect(cardConfigurationDTO.showCvc, true);
+    expect(cardConfigurationDTO.kcpFieldVisibility, FieldVisibility.hide);
+    expect(cardConfigurationDTO.socialSecurityNumberFieldVisibility,
+        FieldVisibility.show);
+    expect(cardConfigurationDTO.supportedCardTypes, ["amex"]);
+  });
+
+  test(
+      "when using google pay configuration, then should parse to GooglePayConfigurationDTO",
+      () {
+    final googlePayConfiguration = GooglePayConfiguration(
+      googlePayEnvironment: GooglePayEnvironment.production,
+      merchantAccount: "GOOGLE_PAY_MERCHANT_ACCOUNT",
+      merchantInfo: MerchantInfo(
+        merchantName: "GOOGLE_PAY_MERCHANT_NAME",
+        merchantId: "GOOGLE_PAY_MERCHANT_ID",
+      ),
+      totalPriceStatus: TotalPriceStatus.finalPrice,
+      allowedCardNetworks: ["AMEX", "DISCOVER", "MASTERCARD", "VISA"],
+      allowedAuthMethods: [CardAuthMethod.cryptogram3DS],
+      allowPrepaidCards: true,
+      allowCreditCards: true,
+      assuranceDetailsRequired: false,
+      emailRequired: true,
+      existingPaymentMethodRequired: true,
+      shippingAddressRequired: true,
+      shippingAddressParameters: ShippingAddressParameters(
+        allowedCountryCodes: ["NL"],
+        isPhoneNumberRequired: true,
+      ),
+      billingAddressRequired: true,
+      billingAddressParameters:
+          BillingAddressParameters(format: "MIN", isPhoneNumberRequired: false),
+    );
+
+    final googlePayConfigurationDTO = googlePayConfiguration.toDTO();
+
+    expect(googlePayConfigurationDTO.googlePayEnvironment,
+        GooglePayEnvironment.production);
+    expect(googlePayConfigurationDTO.merchantAccount,
+        "GOOGLE_PAY_MERCHANT_ACCOUNT");
+    expect(googlePayConfigurationDTO.merchantInfoDTO?.merchantId,
+        "GOOGLE_PAY_MERCHANT_ID");
+    expect(googlePayConfigurationDTO.merchantInfoDTO?.merchantName,
+        "GOOGLE_PAY_MERCHANT_NAME");
+    expect(googlePayConfigurationDTO.totalPriceStatus,
+        TotalPriceStatus.finalPrice);
+    expect(googlePayConfigurationDTO.allowedCardNetworks,
+        ["AMEX", "DISCOVER", "MASTERCARD", "VISA"]);
+    expect(googlePayConfigurationDTO.allowedAuthMethods, ["cryptogram3DS"]);
+    expect(googlePayConfigurationDTO.allowPrepaidCards, true);
+    expect(googlePayConfigurationDTO.allowCreditCards, true);
+    expect(googlePayConfigurationDTO.assuranceDetailsRequired, false);
+    expect(googlePayConfigurationDTO.emailRequired, true);
+    expect(googlePayConfigurationDTO.existingPaymentMethodRequired, true);
+    expect(googlePayConfigurationDTO.shippingAddressRequired, true);
+    expect(
+        googlePayConfigurationDTO
+            .shippingAddressParametersDTO?.allowedCountryCodes,
+        ["NL"]);
+    expect(
+        googlePayConfigurationDTO
+            .shippingAddressParametersDTO?.isPhoneNumberRequired,
+        true);
+    expect(googlePayConfigurationDTO.billingAddressRequired, true);
+    expect(
+        googlePayConfigurationDTO.billingAddressParametersDTO?.format, "MIN");
+    expect(
+        googlePayConfigurationDTO
+            .billingAddressParametersDTO?.isPhoneNumberRequired,
+        false);
+  });
+
+  test(
+      "when using apple pay configuration, then should parse to ApplePayConfigurationDTO",
+      () {
+    final shippingStartDate = DateTime.now();
+    final shippingEndDate = shippingStartDate.add(const Duration(days: 5));
+    final applePayConfiguration = ApplePayConfiguration(
+        merchantId: "APPLE_PAY_MERCHANT_ID",
+        merchantName: "APPLE_PAY_MERCHANT_NAME",
+        allowOnboarding: true,
+        applePaySummaryItems: [
+          ApplePaySummaryItem(
+            label: "Product A",
+            amount: Amount(value: 2599, currency: "EUR"),
+            type: ApplePaySummaryItemType.definite,
+          )
+        ],
+        requiredBillingContactFields: [
+          ApplePayContactField.emailAddress,
+          ApplePayContactField.phoneNumber,
+        ],
+        billingContact: ApplePayContact(
+          emailAddress: "flutterTest@adyen.com",
+          phoneNumber: "0123456789",
+        ),
+        requiredShippingContactFields: [
+          ApplePayContactField.emailAddress,
+          ApplePayContactField.phoneNumber,
+        ],
+        shippingContact: ApplePayContact(
+          emailAddress: "flutterTest@adyen.com",
+          phoneNumber: "9876543210",
+        ),
+        applePayShippingType: ApplePayShippingType.servicePickup,
+        allowShippingContactEditing: true,
+        shippingMethods: [
+          ApplePayShippingMethod(
+            label: "Standard shipping",
+            detail: "DHL",
+            amount: Amount(value: 499, currency: "EUR"),
+            identifier: "Identifier 1",
+            startDate: shippingStartDate,
+            endDate: shippingEndDate,
+          )
+        ],
+        applicationData: null,
+        supportedCountries: ["NL"],
+        merchantCapability: ApplePayMerchantCapability.debit);
+
+    final applePayConfigurationDTO = applePayConfiguration.toDTO();
+
+    expect(applePayConfigurationDTO.merchantId, "APPLE_PAY_MERCHANT_ID");
+    expect(applePayConfigurationDTO.merchantName, "APPLE_PAY_MERCHANT_NAME");
+    expect(applePayConfigurationDTO.allowOnboarding, true);
+    expect(
+        applePayConfigurationDTO.summaryItems?.firstOrNull?.label, "Product A");
+    expect(
+        applePayConfigurationDTO.summaryItems?.firstOrNull?.amount.value, 2599);
+    expect(applePayConfigurationDTO.summaryItems?.firstOrNull?.amount.currency,
+        "EUR");
+    expect(applePayConfigurationDTO.summaryItems?.firstOrNull?.type,
+        ApplePaySummaryItemType.definite);
+    expect(applePayConfigurationDTO.requiredBillingContactFields, [
+      "emailAddress",
+      "phoneNumber",
+    ]);
+    expect(applePayConfigurationDTO.billingContact?.emailAddress,
+        "flutterTest@adyen.com");
+    expect(applePayConfigurationDTO.billingContact?.phoneNumber, "0123456789");
+    expect(applePayConfigurationDTO.requiredShippingContactFields, [
+      "emailAddress",
+      "phoneNumber",
+    ]);
+    expect(applePayConfigurationDTO.shippingContact?.emailAddress,
+        "flutterTest@adyen.com");
+    expect(applePayConfigurationDTO.shippingContact?.phoneNumber, "9876543210");
+    expect(applePayConfigurationDTO.applePayShippingType,
+        ApplePayShippingType.servicePickup);
+    expect(applePayConfigurationDTO.allowShippingContactEditing, true);
+    expect(applePayConfigurationDTO.shippingMethods?.firstOrNull?.label,
+        "Standard shipping");
+    expect(
+        applePayConfigurationDTO.shippingMethods?.firstOrNull?.detail, "DHL");
+    expect(applePayConfigurationDTO.shippingMethods?.firstOrNull?.amount.value,
+        499);
+    expect(
+        applePayConfigurationDTO.shippingMethods?.firstOrNull?.amount.currency,
+        "EUR");
+    expect(applePayConfigurationDTO.shippingMethods?.firstOrNull?.identifier,
+        "Identifier 1");
+    expect(applePayConfigurationDTO.shippingMethods?.firstOrNull?.startDate,
+        shippingStartDate.toIso8601String());
+    expect(applePayConfigurationDTO.shippingMethods?.firstOrNull?.endDate,
+        shippingEndDate.toIso8601String());
+    expect(applePayConfigurationDTO.applicationData, null);
+    expect(applePayConfigurationDTO.supportedCountries, ["NL"]);
+    expect(applePayConfigurationDTO.merchantCapability,
+        ApplePayMerchantCapability.debit);
+  });
+
+  test(
+      "when using cash app pay configuration, then should parse to CashAppPayConfigurationDTO",
+      () {
+    const cashAppPayConfiguration = CashAppPayConfiguration(
+      cashAppPayEnvironment: CashAppPayEnvironment.production,
+      returnUrl: "RETURN_URL",
+    );
+
+    final cashAppPayConfigurationDTO = cashAppPayConfiguration.toDTO();
+
+    expect(cashAppPayConfigurationDTO.cashAppPayEnvironment,
+        CashAppPayEnvironment.production);
+    expect(cashAppPayConfigurationDTO.returnUrl, "RETURN_URL");
   });
 }
