494a15f83a503910028bd94f7631f46217180dbb
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2024-06-26 11:30:48 +0200

    Added example for multi component session flow

--- example/lib/screens/component/multi_component/multi_component_navigation_screen.dart
+++ example/lib/screens/component/multi_component/multi_component_navigation_screen.dart
@@ -13,12 +13,11 @@ class MultiComponentNavigationScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // This is not yet supported
-              // TextButton(
-              //   child: const Text("Multi component session"),
-              //   onPressed: () =>
-              //       Navigator.pushNamed(context, "/multiComponentSessionScreen"),
-              // ),
+              TextButton(
+                child: const Text("Multi component session"),
+                onPressed: () => Navigator.pushNamed(
+                    context, "/multiComponentSessionScreen"),
+              ),
               TextButton(
                 child: const Text("Multi component advanced"),
                 onPressed: () => Navigator.pushNamed(
