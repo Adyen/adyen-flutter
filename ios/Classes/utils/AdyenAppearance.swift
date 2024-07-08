7c94bfd046c6b0650d5d23f3cf8edc3c48add9e8
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2024-07-08 11:16:44 +0200

    Migrated AdyenAppearance to an enum to simplify ui customization

--- ios/Classes/utils/AdyenAppearance.swift
+++ ios/Classes/utils/AdyenAppearance.swift
@@ -1,63 +1,16 @@
-@_spi(AdyenInternal) import Adyen
-import Foundation
+import Adyen
 
-/// Describes class that provides customization to Drop-in UI elements.
-public protocol AdyenDropInAppearanceProvider: AnyObject {
+public enum AdyenAppearance {
+   private static var _dropInStyle: Adyen.DropInComponent.Style = Adyen.DropInComponent.Style()
+   private static var _cardComponentStyle: Adyen.FormComponentStyle = Adyen.FormComponentStyle()
 
-    /// Implement this method to apply the style to Drop-In.
-    /// Uses Drop-In Component Style as an umbrella style.
-    static func createDropInStyle() -> Adyen.DropInComponent.Style
-}
+   public static var dropInStyle: Adyen.DropInComponent.Style {
+      get { return _dropInStyle }
+      set { _dropInStyle = newValue }
+   }
 
-/// Describes class that provides customization to Component UI elements.
-public protocol AdyenComponentAppearanceProvider: AnyObject {
-
-    /// Implement this method to apply the style to the card component.
-    /// Uses Form Component Style as an umbrella style.
-    static func createCardComponentStyle() -> Adyen.FormComponentStyle
-}
-
-extension AdyenDropInAppearanceProvider {
-    static func createDropInStyle() -> Adyen.DropInComponent.Style {
-        DropInComponent.Style()
-    }
-}
-
-extension AdyenComponentAppearanceProvider {
-    static func createCardComponentStyle() -> Adyen.FormComponentStyle {
-        FormComponentStyle()
-    }
-}
-
-internal class AdyenAppearanceLoader: NSObject {
-    private static let expectedClassName = "AdyenAppearance"
-    private static let bundleExecutableKey = "CFBundleExecutable"
-
-    static func findDropInStyle() -> Adyen.DropInComponent.Style? {
-        let appearanceProviders: [AnyClass?] = findAppearanceProviders()
-        let dropInAppearanceProviders = appearanceProviders.compactMap { $0 as? AdyenDropInAppearanceProvider.Type }
-        guard let dropInAppearanceProvider = dropInAppearanceProviders.first else {
-            return nil
-        }
-        
-        return dropInAppearanceProvider.createDropInStyle()
-    }
-    
-    static func findCardComponentStyle() -> Adyen.FormComponentStyle? {
-        let appearanceProviders: [AnyClass?] = findAppearanceProviders()
-        let cardComponentAppearanceProviders = appearanceProviders.compactMap { $0 as? AdyenComponentAppearanceProvider.Type }
-        guard let cardComponentAppearanceProvider = cardComponentAppearanceProviders.first else {
-            return nil
-        }
-        
-        return cardComponentAppearanceProvider.createCardComponentStyle()
-    }
-    
-    private static func findAppearanceProviders() -> [AnyClass?] {
-        return Bundle.allBundles
-            .compactMap { $0.infoDictionary?[bundleExecutableKey] as? String }
-            .map { $0.replacingOccurrences(of: " ", with: "_") }
-            .map { $0.replacingOccurrences(of: "-", with: "_") }
-            .compactMap { NSClassFromString("\($0).\(expectedClassName)") }
-    }
+   public static var cardComponentStyle: Adyen.FormComponentStyle {
+      get { return _cardComponentStyle }
+      set { _cardComponentStyle = newValue }
+   }
 }
