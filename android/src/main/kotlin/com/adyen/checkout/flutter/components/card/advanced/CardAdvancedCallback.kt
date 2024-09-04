b9701c08a1090682186f0ec18434baa841cd644b
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2024-09-04 10:49:55 +0200

    Removed unused import

--- android/src/main/kotlin/com/adyen/checkout/flutter/components/card/advanced/CardAdvancedCallback.kt
+++ android/src/main/kotlin/com/adyen/checkout/flutter/components/card/advanced/CardAdvancedCallback.kt
@@ -2,7 +2,6 @@ package com.adyen.checkout.flutter.components.card.advanced
 
 import ComponentFlutterInterface
 import com.adyen.checkout.card.CardComponentState
-import com.adyen.checkout.components.core.ComponentError
 import com.adyen.checkout.flutter.components.base.ComponentAdvancedCallback
 
 internal class CardAdvancedCallback(
