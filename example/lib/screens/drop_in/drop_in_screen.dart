5e0fe4b518d13ce8869f43d201b74aeaddf0dc65
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2024-10-14 11:37:56 +0200

    Changed ideal payment method name example

--- example/lib/screens/drop_in/drop_in_screen.dart
+++ example/lib/screens/drop_in/drop_in_screen.dart
@@ -138,7 +138,7 @@ class DropInScreen extends StatelessWidget {
       cashAppPayConfiguration: cashAppPayConfiguration,
       storedPaymentMethodConfiguration: storedPaymentMethodConfiguration,
       paymentMethodNames: {
-        "scheme": "Credit card",
+        "ideal": "Pay with iDEAL",
       },
     );
 
