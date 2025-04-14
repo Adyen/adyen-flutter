a6b1fb1a6e5895875a200b655fad409e4295157e
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2025-04-14 10:03:22 +0200

    Removed unused parseMonthYear method and applied code formatter

--- example/lib/screens/api_only/input_formatters/card_number_input_formatter.dart
+++ example/lib/screens/api_only/input_formatters/card_number_input_formatter.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/services.dart';
 
 class CardNumberInputFormatter extends TextInputFormatter {
+  static const int _groupSize = 4;
+  static const String _separator = ' ';
+
   @override
   TextEditingValue formatEditUpdate(
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    var text = newValue.text;
+    final String newText = newValue.text;
 
     if (newValue.selection.baseOffset == 0) {
       return newValue;
     }
 
     var buffer = StringBuffer();
-    for (int i = 0; i < text.length; i++) {
-      buffer.write(text[i]);
+    for (int i = 0; i < newText.length; i++) {
+      buffer.write(newText[i]);
       var nonZeroIndex = i + 1;
-      if (nonZeroIndex % 4 == 0 && nonZeroIndex != text.length) {
-        buffer.write(' ');
+      if (nonZeroIndex % _groupSize == 0 && nonZeroIndex != newText.length) {
+        buffer.write(_separator);
       }
     }
 
--- example/lib/screens/api_only/input_formatters/month_year_input_formatter.dart
+++ example/lib/screens/api_only/input_formatters/month_year_input_formatter.dart
@@ -3,33 +3,37 @@ import 'package:flutter/services.dart';
 class MonthYearInputFormatter extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    final text = newValue.text.replaceAll(RegExp(r'\D'), '');
-    final textLength = text.length;
-    final oldLength = oldValue.text.length;
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final String newText = newValue.text.replaceAll(RegExp(r'\D'), '');
+    final int newTextLength = newText.length;
+    final int oldTextLength = oldValue.text.length;
 
-    if (textLength == 0) return newValue.copyWith(text: '');
-    if (textLength == 1 &&
-        int.tryParse(text) != null &&
-        int.parse(text) >= 2 &&
-        oldLength == 0) {
+    if (newText.isEmpty) {
+      return newValue.copyWith(text: '');
+    }
+
+    if (newTextLength == 1 &&
+        int.tryParse(newText) != null &&
+        int.parse(newText) >= 2 &&
+        oldTextLength == 0) {
       return newValue.copyWith(
-        text: '0$text',
+        text: '0$newText',
         selection: const TextSelection.collapsed(offset: 2),
       );
     }
-    if (textLength > 4) return oldValue;
 
-    final formattedText =
-        textLength > 2 ? '${text.substring(0, 2)}/${text.substring(2)}' : text;
+    if (newTextLength > 4) {
+      return oldValue;
+    }
+
+    final formattedText = newTextLength > 2
+        ? '${newText.substring(0, 2)}/${newText.substring(2)}'
+        : newText;
     return newValue.copyWith(
       text: formattedText,
       selection: TextSelection.collapsed(offset: formattedText.length),
     );
   }
-
-  static String parseMonthYear(String formattedDate) =>
-      formattedDate.replaceAll(RegExp(r'\D'), '').length == 4
-          ? formattedDate.replaceAll(RegExp(r'\D'), '')
-          : '';
 }
