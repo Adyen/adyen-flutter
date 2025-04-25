d09b7c42bc3c76c8f5df9786922af027722c2817
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2025-04-25 16:27:04 +0200

    Adjusted example style on iOS

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -22,7 +22,10 @@ import UIKit
 
     private func setDropInStyle() {
         var dropInStyle = Adyen.DropInComponent.Style()
-        dropInStyle.formComponent.mainButtonItem.button.backgroundColor = .black
+        dropInStyle.navigation.backgroundColor = color(hex: 0x287F99)
+        dropInStyle.formComponent.backgroundColor = color(hex: 0x287F99)
+        dropInStyle.formComponent.textField.tintColor = color(hex: 0x06F01D)
+        dropInStyle.formComponent.mainButtonItem.button.backgroundColor = color(hex: 0xFFB860)
         dropInStyle.formComponent.mainButtonItem.button.title.color = .white
         AdyenAppearance.dropInStyle = dropInStyle
     }
@@ -32,5 +35,18 @@ import UIKit
         cardComponentStyle.mainButtonItem.button.backgroundColor = .black
         cardComponentStyle.mainButtonItem.button.title.color = .white
         AdyenAppearance.cardComponentStyle = cardComponentStyle
+    }
+    
+    internal func color(hex: UInt) -> UIColor {
+        assert(
+            hex >= 0x000000 && hex <= 0xFFFFFF,
+            "Invalid Hexadecimal color, Hexadecimal number should be between 0x0 and 0xFFFFFF"
+        )
+        return UIColor(
+            red: CGFloat((hex >> 16) & 0xFF) / 255.0,
+            green: CGFloat((hex >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(hex & 0xFF) / 255.0,
+            alpha: 1.0
+        )
     }
 }
