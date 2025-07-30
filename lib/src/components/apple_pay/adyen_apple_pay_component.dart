58556c5f9ba67d583a233580f8663960b1577939
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2025-07-30 09:19:40 +0200

    Refactored ApplePay advanced flow widget
    The `_ApplePayAdvancedFlowWidget` has been removed and its logic integrated directly into the `AdyenApplePayComponent`'s build method. This simplifies the component structure. Additionally, the switch expressions have been converted to switch statements for better readability.

--- lib/src/components/apple_pay/adyen_apple_pay_component.dart
+++ lib/src/components/apple_pay/adyen_apple_pay_component.dart
@@ -37,9 +37,11 @@ class AdyenApplePayComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return switch (defaultTargetPlatform) {
-      TargetPlatform.iOS => switch (checkout) {
-          SessionCheckout it => ApplePaySessionComponent(
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+        switch (checkout) {
+          case SessionCheckout it:
+            return ApplePaySessionComponent(
               key: key,
               session: it.toDTO(),
               applePayPaymentMethod: json.encode(paymentMethod),
@@ -53,13 +55,19 @@ class AdyenApplePayComponent extends StatelessWidget {
               loadingIndicator: loadingIndicator,
               onUnavailable: onUnavailable,
               unavailableWidget: unavailableWidget,
-            ),
-          AdvancedCheckout it => _ApplePayAdvancedFlowWidget(
+            );
+          case AdvancedCheckout it:
+            if (configuration.amount == null) {
+              AdyenLogger.instance.print("Apple Pay requires to set an amount when using the advanced flow.");
+              onUnavailable?.call();
+              return unavailableWidget ?? const SizedBox.shrink();
+            }
+            return ApplePayAdvancedComponent(
               key: key,
-              paymentMethod: paymentMethod,
-              configuration: configuration,
+              applePayPaymentMethod: json.encode(paymentMethod),
+              applePayComponentConfiguration: configuration,
               onPaymentResult: onPaymentResult,
-              checkout: it,
+              advancedCheckout: it,
               style: _mapToApplePayButtonStyle(),
               type: _mapToApplePayButtonType(),
               width: _determineWidth(),
@@ -68,10 +76,11 @@ class AdyenApplePayComponent extends StatelessWidget {
               loadingIndicator: loadingIndicator,
               onUnavailable: onUnavailable,
               unavailableWidget: unavailableWidget,
-            ),
-        },
-      _ => throw Exception("The Apple Pay component is not supported on $defaultTargetPlatform"),
-    };
+            );
+        }
+      default:
+        throw Exception("The Apple Pay component is not supported on $defaultTargetPlatform");
+    }
   }
 
   double _determineWidth() {
@@ -120,61 +129,5 @@ class AdyenApplePayComponent extends StatelessWidget {
       ApplePayButtonType.contribute => pay_sdk.ApplePayButtonType.contribute,
       ApplePayButtonType.tip => pay_sdk.ApplePayButtonType.tip,
     };
-  }
-}
-
-class _ApplePayAdvancedFlowWidget extends StatelessWidget {
-  const _ApplePayAdvancedFlowWidget({
-    super.key,
-    required this.paymentMethod,
-    required this.configuration,
-    required this.onPaymentResult,
-    required this.checkout,
-    required this.style,
-    required this.type,
-    required this.width,
-    required this.height,
-    this.cornerRadius,
-    this.loadingIndicator,
-    this.unavailableWidget,
-    this.onUnavailable,
-  });
-
-  final Map<String, dynamic> paymentMethod;
-  final ApplePayComponentConfiguration configuration;
-  final Function(PaymentResult result) onPaymentResult;
-  final AdvancedCheckout checkout;
-  final pay_sdk.ApplePayButtonStyle style;
-  final pay_sdk.ApplePayButtonType type;
-  final double width;
-  final double height;
-  final double? cornerRadius;
-  final Widget? loadingIndicator;
-  final Widget? unavailableWidget;
-  final Function()? onUnavailable;
-
-  @override
-  Widget build(BuildContext context) {
-    if (configuration.amount == null) {
-      AdyenLogger.instance.print("Apple Pay requires to set an amount when using the advanced flow.");
-      onUnavailable?.call();
-      return unavailableWidget ?? const SizedBox.shrink();
-    }
-
-    return ApplePayAdvancedComponent(
-      key: key,
-      applePayPaymentMethod: json.encode(paymentMethod),
-      applePayComponentConfiguration: configuration,
-      onPaymentResult: onPaymentResult,
-      advancedCheckout: checkout,
-      style: style,
-      type: type,
-      width: width,
-      height: height,
-      cornerRadius: cornerRadius,
-      loadingIndicator: loadingIndicator,
-      onUnavailable: onUnavailable,
-      unavailableWidget: unavailableWidget,
-    );
   }
 }
