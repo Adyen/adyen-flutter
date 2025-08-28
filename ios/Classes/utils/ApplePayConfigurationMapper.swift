7ff9872a2fc5df90b6ef62c05cc2546291075b4d
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2025-08-28 16:38:08 +0200

    Applied changes from code review

--- ios/Classes/utils/ApplePayConfigurationMapper.swift
+++ ios/Classes/utils/ApplePayConfigurationMapper.swift
@@ -18,11 +18,25 @@ extension ApplePayConfigurationDTO {
         paymentRequest.billingContact = billingContact?.toApplePayContact()
         paymentRequest.shippingContact = shippingContact?.toApplePayContact()
         paymentRequest.merchantCapabilities = merchantCapability.toMerchantCapability()
-        requiredShippingContactFields.map { paymentRequest.requiredShippingContactFields = mapToContactFields(contactFields: $0) }
-        requiredBillingContactFields.map { paymentRequest.requiredBillingContactFields = mapToContactFields(contactFields: $0) }
-        applePayShippingType.map { paymentRequest.shippingType = $0.toPKShippingType() }
-        supportedCountries.map { paymentRequest.supportedCountries = .init($0.compactMap { $0 }) }
-        try shippingMethods.map { paymentRequest.shippingMethods = try $0.compactMap { try $0?.toPKShippingMethod() }}
+        if let requiredShippingContactFields = requiredShippingContactFields {
+            paymentRequest.requiredShippingContactFields = mapToContactFields(contactFields: requiredShippingContactFields)
+        }
+
+        if let requiredBillingContactFields = requiredBillingContactFields {
+            paymentRequest.requiredBillingContactFields = mapToContactFields(contactFields: requiredBillingContactFields)
+        }
+
+        if let applePayShippingType = applePayShippingType {
+            paymentRequest.shippingType = applePayShippingType.toPKShippingType()
+        }
+
+        if let supportedCountries = supportedCountries {
+            paymentRequest.supportedCountries = .init(supportedCountries.compactMap { $0 })
+        }
+
+        if let shippingMethods = shippingMethods {
+            paymentRequest.shippingMethods = try shippingMethods.compactMap { try $0?.toPKShippingMethod() }
+        }
         
         if #available(iOS 15.0, *) {
             allowShippingContactEditing.map {
