364d9f2ea70ca68fd7e18cf4bbe65147e8627a59
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2025-08-29 15:19:30 +0200

    Updated iOS SDK to v5.20.0

--- ios/adyen_checkout/Package.swift
+++ ios/adyen_checkout/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "adyen-checkout", targets: ["adyen_checkout"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Adyen/adyen-ios", exact: "5.19.2")
+        .package(url: "https://github.com/Adyen/adyen-ios", exact: "5.20.0")
     ],
     targets: [
         .target(
