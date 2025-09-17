dcba5010de5fed616bf292736f24d7de674e6dbf
Robert Schulze Dieckhoff <13377878+Robert-SD@users.noreply.github.com>
2025-09-17 10:29:30 +0200

    Add SPM support (#478)
    * Started with SPM migration
    
    * Added remaining SDK dependencies
    
    * Added canImport checks
    
    * Added spm flag to pubspec yaml and improved pod integration
    
    * Added SPM test action
    
    * Fixed workflow
    
    * Added permissions
    
    * Updated iOS SDK to v5.20.0
    
    * Added example build with SPM
    
    * Removed redundant dependencies
    
    * Applied swift format
    
    * Added changelog entry and adjusted docs

---
+++ ios/adyen_checkout/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "adyen_checkout",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(name: "adyen-checkout", targets: ["adyen_checkout"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Adyen/adyen-ios", exact: "5.20.0")
+    ],
+    targets: [
+        .target(
+            name: "adyen_checkout",
+            dependencies: [
+                .product(name: "AdyenDropIn", package: "adyen-ios"),
+                .product(name: "AdyenSession", package: "adyen-ios")
+            ],
+            resources: []
+        )
+    ]
+)
---
+++ ios/adyen_checkout/Sources/adyen_checkout/components/instant/InstantComponentManager.swift
@@ -0,0 +1,71 @@
+import Adyen
+import Foundation
+
+class InstantComponentManager {
+    private let componentFlutterApi: ComponentFlutterInterface
+    private let sessionHolder: SessionHolder
+    private var instantComponent: InstantComponentProtocol?
+    enum Constants {
+        static let instantSessionComponentId = "INSTANT_SESSION_COMPONENT"
+        static let instantAdvancedComponentId = "INSTANT_ADVANCED_COMPONENT"
+    }
+    
+    init(componentFlutterApi: ComponentFlutterInterface, sessionHolder: SessionHolder) {
+        self.componentFlutterApi = componentFlutterApi
+        self.sessionHolder = sessionHolder
+    }
+    
+    func startInstantComponent(
+        instantPaymentConfigurationDTO: InstantPaymentConfigurationDTO,
+        encodedPaymentMethod: String,
+        componentId: String
+    ) {
+        do {
+            let paymentMethod = try JSONDecoder().decode(InstantPaymentMethod.self, from: Data(encodedPaymentMethod.utf8))
+            let adyenContext = try instantPaymentConfigurationDTO.createAdyenContext()
+            if componentId.contains(Constants.instantAdvancedComponentId) {
+                instantComponent = InstantAdvancedComponent(
+                    componentFlutterApi: componentFlutterApi,
+                    paymentMethod: paymentMethod,
+                    adyenContext: adyenContext,
+                    componentId: componentId
+                )
+            } else if componentId.contains(Constants.instantSessionComponentId) {
+                instantComponent = InstantSessionComponent(
+                    componentFlutterApi: componentFlutterApi,
+                    paymentMethod: paymentMethod,
+                    adyenContext: adyenContext,
+                    sessionHolder: sessionHolder,
+                    componentId: componentId
+                )
+            } else {
+                throw PlatformError(errorDescription: "Instant component not available for payment flow.")
+            }
+            instantComponent?.initiatePayment()
+        } catch {
+            let componentCommunicationModel = ComponentCommunicationModel(
+                type: ComponentCommunicationType.result,
+                componentId: componentId,
+                paymentResult: PaymentResultDTO(
+                    type: PaymentResultEnum.error,
+                    reason: error.localizedDescription
+                )
+            )
+            self.componentFlutterApi.onComponentCommunication(
+                componentCommunicationModel: componentCommunicationModel,
+                completion: { _ in }
+            )
+        }
+    }
+    
+    func handlePaymentEvent(paymentEventDTO: PaymentEventDTO) {
+        if let instantAdvancedComponent = instantComponent as? InstantAdvancedComponent {
+            instantAdvancedComponent.handlePaymentEvent(paymentEventDTO: paymentEventDTO)
+        }
+    }
+    
+    func onDispose() {
+        instantComponent?.onDispose()
+        instantComponent = nil
+    }
+}
