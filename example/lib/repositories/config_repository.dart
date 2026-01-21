56f7c85aa01d9e0fedd320050b83bc61f1caf3d3
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2026-01-21 13:19:20 +0100

    Add base 64 decoding to config

--- example/lib/repositories/config_repository.dart
+++ example/lib/repositories/config_repository.dart
@@ -7,6 +7,8 @@ import 'package:flutter_launch_arguments/flutter_launch_arguments.dart';
 class ConfigRepository {
   final String launchConfigKey = "config";
   final String cardConfigurationKey = "CARD_CONFIGURATION";
+  final FlutterLaunchArguments flutterLaunchArguments =
+      FlutterLaunchArguments();
 
   Future<CardConfiguration> loadCardConfiguration() async {
     final String? launchConfigString = await _loadLaunchConfig();
@@ -17,6 +19,18 @@ class ConfigRepository {
     return CardConfigurationExtension.fromJson(cardConfigJson);
   }
 
-  Future<String?> _loadLaunchConfig() async =>
-      await FlutterLaunchArguments().getString(launchConfigKey);
+  Future<String?> _loadLaunchConfig() async {
+    final configBase64 =
+        await flutterLaunchArguments.getString(launchConfigKey);
+    print("Raw: $configBase64");
+
+    if (configBase64 == null) {
+      return null;
+    }
+
+    List<int> bytes = base64.decode(configBase64);
+    String jsonString = utf8.decode(bytes);
+    print("Config JSON: $jsonString");
+    return jsonString;
+  }
 }
