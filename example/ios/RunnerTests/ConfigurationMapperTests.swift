0dbda346b907b03688f3adb40b309475622241cc
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2026-04-13 14:57:37 +0200

    Applied swiftformat

--- example/ios/RunnerTests/ConfigurationMapperTests.swift
+++ example/ios/RunnerTests/ConfigurationMapperTests.swift
@@ -1,168 +1,169 @@
 @_spi(AdyenInternal) import Adyen
+@testable import adyen_checkout
 import XCTest
 
-@testable import adyen_checkout
-
 #if canImport(AdyenCard)
-  import AdyenCard
+    import AdyenCard
 #endif
 #if canImport(Adyen3DS2)
-  import Adyen3DS2
+    import Adyen3DS2
 #endif
 
 final class ConfigurationMapperTests: XCTestCase {
-  // MARK: - Environment Mapping Tests
+    // MARK: - Environment Mapping Tests
 
-  func test_mapToEnvironment_shouldMapAllEnvironmentsCorrectly() {
-    let testCases: [(environment: adyen_checkout.Environment, expected: Adyen.Environment)] = [
-      (.test, .test),
-      (.europe, .liveEurope),
-      (.unitedStates, .liveUnitedStates),
-      (.australia, .liveAustralia),
-      (.india, .liveIndia),
-      (.apse, .liveApse),
-      (.nea, .liveNea),
-    ]
+    func test_mapToEnvironment_shouldMapAllEnvironmentsCorrectly() {
+        let testCases: [(environment: adyen_checkout.Environment, expected: Adyen.Environment)] = [
+            (.test, .test),
+            (.europe, .liveEurope),
+            (.unitedStates, .liveUnitedStates),
+            (.australia, .liveAustralia),
+            (.india, .liveIndia),
+            (.apse, .liveApse),
+            (.nea, .liveNea),
+        ]
 
-    for testCase in testCases {
-      let result = testCase.environment.mapToEnvironment()
-      XCTAssertEqual(result, testCase.expected, "Failed for environment: \(testCase.environment)")
+        for testCase in testCases {
+            let result = testCase.environment.mapToEnvironment()
+            XCTAssertEqual(result, testCase.expected, "Failed for environment: \(testCase.environment)")
+        }
     }
-  }
 
-  // MARK: - Amount Mapping Tests
+    // MARK: - Amount Mapping Tests
 
-  func test_mapToAmount_withEUR_shouldMapCorrectly() {
-    let sut = AmountDTO(currency: "EUR", value: 1000)
+    func test_mapToAmount_withEUR_shouldMapCorrectly() {
+        let sut = AmountDTO(currency: "EUR", value: 1000)
 
-    let result = sut.mapToAmount()
+        let result = sut.mapToAmount()
 
-    XCTAssertEqual(result.currencyCode, "EUR")
-    XCTAssertEqual(result.value, 1000)
-  }
+        XCTAssertEqual(result.currencyCode, "EUR")
+        XCTAssertEqual(result.value, 1000)
+    }
 
-  func test_mapToAmount_withUSD_shouldMapCorrectly() {
-    let sut = AmountDTO(currency: "USD", value: 2500)
+    func test_mapToAmount_withUSD_shouldMapCorrectly() {
+        let sut = AmountDTO(currency: "USD", value: 2500)
 
-    let result = sut.mapToAmount()
+        let result = sut.mapToAmount()
 
-    XCTAssertEqual(result.currencyCode, "USD")
-    XCTAssertEqual(result.value, 2500)
-  }
+        XCTAssertEqual(result.currencyCode, "USD")
+        XCTAssertEqual(result.value, 2500)
+    }
 
-  // MARK: - FieldVisibility Mapping Tests
+    // MARK: - FieldVisibility Mapping Tests
 
-  func test_toCardFieldVisibility_withShow_shouldReturnShow() {
-    let sut = FieldVisibility.show
+    func test_toCardFieldVisibility_withShow_shouldReturnShow() {
+        let sut = FieldVisibility.show
 
-    let result = sut.toCardFieldVisibility()
+        let result = sut.toCardFieldVisibility()
 
-    XCTAssertEqual(result, CardComponent.FieldVisibility.show)
-  }
+        XCTAssertEqual(result, CardComponent.FieldVisibility.show)
+    }
 
-  func test_toCardFieldVisibility_withHide_shouldReturnHide() {
-    let sut = FieldVisibility.hide
+    func test_toCardFieldVisibility_withHide_shouldReturnHide() {
+        let sut = FieldVisibility.hide
 
-    let result = sut.toCardFieldVisibility()
+        let result = sut.toCardFieldVisibility()
 
-    XCTAssertEqual(result, CardComponent.FieldVisibility.hide)
-  }
+        XCTAssertEqual(result, CardComponent.FieldVisibility.hide)
+    }
 
-  // MARK: - ThreeDS2Configuration Mapping Tests
+    // MARK: - ThreeDS2Configuration Mapping Tests
 
-  func test_mapToThreeDS2Configuration_withValidURL_shouldSetRequestorAppURL() {
-    let sut = ThreeDS2ConfigurationDTO(requestorAppURL: "https://example.com/3ds2")
+    func test_mapToThreeDS2Configuration_withValidURL_shouldSetRequestorAppURL() {
+        let sut = ThreeDS2ConfigurationDTO(requestorAppURL: "https://example.com/3ds2")
 
-    let result = sut.mapToThreeDS2Configuration()
+        let result = sut.mapToThreeDS2Configuration()
 
-    XCTAssertEqual(result.requestorAppURL?.absoluteString, "https://example.com/3ds2")
-  }
+        XCTAssertEqual(result.requestorAppURL?.absoluteString, "https://example.com/3ds2")
+    }
 
-  func test_mapToThreeDS2Configuration_withInvalidURL_shouldReturnNilRequestorAppURL() {
-    let sut = ThreeDS2ConfigurationDTO(requestorAppURL: "")
+    func test_mapToThreeDS2Configuration_withInvalidURL_shouldReturnNilRequestorAppURL() {
+        let sut = ThreeDS2ConfigurationDTO(requestorAppURL: "")
 
-    let result = sut.mapToThreeDS2Configuration()
+        let result = sut.mapToThreeDS2Configuration()
 
-    XCTAssertNil(result.requestorAppURL)
-  }
+        XCTAssertNil(result.requestorAppURL)
+    }
 
-  // MARK: - PaymentResultEnum Mapping Tests
+    // MARK: - PaymentResultEnum Mapping Tests
 
-  func test_fromError_withComponentCancelled_shouldReturnCancelledByUser() {
-    let sut = ComponentError.cancelled
+    func test_fromError_withComponentCancelled_shouldReturnCancelledByUser() {
+        let sut = ComponentError.cancelled
 
-    let result = PaymentResultEnum.from(error: sut)
+        let result = PaymentResultEnum.from(error: sut)
 
-    XCTAssertEqual(result, PaymentResultEnum.cancelledByUser)
-  }
+        XCTAssertEqual(result, PaymentResultEnum.cancelledByUser)
+    }
 
-  func test_fromError_withGenericError_shouldReturnError() {
-    let sut = NSError(domain: "TestDomain", code: 123, userInfo: nil)
+    func test_fromError_withGenericError_shouldReturnError() {
+        let sut = NSError(domain: "TestDomain", code: 123, userInfo: nil)
 
-    let result = PaymentResultEnum.from(error: sut)
+        let result = PaymentResultEnum.from(error: sut)
 
-    XCTAssertEqual(result, PaymentResultEnum.error)
-  }
+        XCTAssertEqual(result, PaymentResultEnum.error)
+    }
 
-  func test_fromError_withThreeDS2ChallengeCancelled_shouldReturnCancelledByUser() {
-    let sut = NSError(
-      domain: ADYRuntimeErrorDomain,
-      code: Int(ADYRuntimeErrorCode.challengeCancelled.rawValue),
-      userInfo: nil
-    )
+    func test_fromError_withThreeDS2ChallengeCancelled_shouldReturnCancelledByUser() {
+        let sut = NSError(
+            domain: ADYRuntimeErrorDomain,
+            code: Int(ADYRuntimeErrorCode.challengeCancelled.rawValue),
+            userInfo: nil
+        )
 
-    let result = PaymentResultEnum.from(error: sut)
+        let result = PaymentResultEnum.from(error: sut)
 
-    XCTAssertEqual(result, PaymentResultEnum.cancelledByUser)
-  }
+        XCTAssertEqual(result, PaymentResultEnum.cancelledByUser)
+    }
 
-  // MARK: - ActionComponentConfigurationDTO Tests
+    // MARK: - ActionComponentConfigurationDTO Tests
 
-  func test_createAdyenContext_withDefaultsFromActionComponent_shouldCreateContext() throws {
-    let sut = createActionComponentConfigurationDTO()
+    func test_createAdyenContext_withDefaultsFromActionComponent_shouldCreateContext() throws {
+        let sut = createActionComponentConfigurationDTO()
 
-    let result = try sut.createAdyenContext()
+        let result = try sut.createAdyenContext()
 
-    XCTAssertNotNil(result)
-    XCTAssertNotNil(result.apiContext)
-  }
+        XCTAssertNotNil(result)
+        XCTAssertNotNil(result.apiContext)
+    }
 
-  func test_createAdyenContext_withShopperLocale_shouldCreateContext() throws {
-    let sut = createActionComponentConfigurationDTO(shopperLocale: "nl-NL")
+    func test_createAdyenContext_withShopperLocale_shouldCreateContext() throws {
+        let sut = createActionComponentConfigurationDTO(shopperLocale: "nl-NL")
 
-    let result = try sut.createAdyenContext()
+        let result = try sut.createAdyenContext()
 
-    XCTAssertNotNil(result)
-  }
+        XCTAssertNotNil(result)
+    }
 
-  func test_createAdyenContext_withoutShopperLocale_shouldCreateContext() throws {
-    let sut = createActionComponentConfigurationDTO(shopperLocale: nil)
+    func test_createAdyenContext_withoutShopperLocale_shouldCreateContext() throws {
+        let sut = createActionComponentConfigurationDTO(shopperLocale: nil)
 
-    let result = try sut.createAdyenContext()
+        let result = try sut.createAdyenContext()
 
-    XCTAssertNotNil(result)
-  }
+        XCTAssertNotNil(result)
+    }
 
-  func
-    test_createActionComponentConfiguration_withThreeDS2RequestorAppURL_shouldCreateConfiguration()
-  {
-    let sut = createActionComponentConfigurationDTO(
-      threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO(
-        requestorAppURL: "https://example.com/action-3ds2")
-    )
+    func
+        test_createActionComponentConfiguration_withThreeDS2RequestorAppURL_shouldCreateConfiguration()
+    {
+        let sut = createActionComponentConfigurationDTO(
+            threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO(
+                requestorAppURL: "https://example.com/action-3ds2"
+            )
+        )
 
-    let result = sut.threeDS2ConfigurationDTO?.buildActionComponentConfiguration()
+        let result = sut.threeDS2ConfigurationDTO?.buildActionComponentConfiguration()
 
-    XCTAssertNotNil(result)
-    XCTAssertEqual(
-      result?.threeDS.requestorAppURL?.absoluteString, "https://example.com/action-3ds2")
-  }
+        XCTAssertNotNil(result)
+        XCTAssertEqual(
+            result?.threeDS.requestorAppURL?.absoluteString, "https://example.com/action-3ds2"
+        )
+    }
 
-  func test_createActionComponentConfiguration_withoutThreeDS2_shouldReturnNil() {
-    let sut = createActionComponentConfigurationDTO(threeDS2ConfigurationDTO: nil)
+    func test_createActionComponentConfiguration_withoutThreeDS2_shouldReturnNil() {
+        let sut = createActionComponentConfigurationDTO(threeDS2ConfigurationDTO: nil)
 
-    let result = sut.threeDS2ConfigurationDTO?.buildActionComponentConfiguration()
+        let result = sut.threeDS2ConfigurationDTO?.buildActionComponentConfiguration()
 
-    XCTAssertNil(result)
-  }
+        XCTAssertNil(result)
+    }
 }
--- example/ios/RunnerTests/TestUtils.swift
+++ example/ios/RunnerTests/TestUtils.swift
@@ -1,132 +1,131 @@
 @_spi(AdyenInternal) import Adyen
+@testable import adyen_checkout
 @_spi(AdyenInternal) import AdyenNetworking
 
-@testable import adyen_checkout
-
 #if canImport(AdyenCard)
-  import AdyenCard
+    import AdyenCard
 #endif
 #if canImport(AdyenEncryption)
-  import AdyenEncryption
+    import AdyenEncryption
 #endif
 #if canImport(AdyenActions)
-  import AdyenActions
+    import AdyenActions
 #endif
 #if canImport(Adyen3DS2)
-  import Adyen3DS2
+    import Adyen3DS2
 #endif
 
 extension CardComponent.AddressFormType: Equatable {
-  public static func == (lhs: Self, rhs: Self) -> Bool {
-    switch (lhs, rhs) {
-    case (.full, .full):
-      return true
-    case (.none, .none):
-      return true
-    case (.postalCode, .postalCode):
-      return true
-    default:
-      return false
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case (.full, .full):
+            return true
+        case (.none, .none):
+            return true
+        case (.postalCode, .postalCode):
+            return true
+        default:
+            return false
+        }
     }
-  }
 }
 
 let testClientKey = "test_qwertyuiopasdfghjklzxcvbnmqwerty"
 
 func createDropInConfigurationDTO(
-  environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
-  clientKey: String = testClientKey,
-  countryCode: String = "NL",
-  amount: AmountDTO? = AmountDTO(currency: "EUR", value: 1000),
-  shopperLocale: String? = nil,
-  cardConfigurationDTO: CardConfigurationDTO? = nil,
-  applePayConfigurationDTO: ApplePayConfigurationDTO? = nil,
-  cashAppPayConfigurationDTO: CashAppPayConfigurationDTO? = nil,
-  twintConfigurationDTO: TwintConfigurationDTO? = nil,
-  threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO? = nil,
-  analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0"),
-  showPreselectedStoredPaymentMethod: Bool = true,
-  skipListWhenSinglePaymentMethod: Bool = false,
-  isRemoveStoredPaymentMethodEnabled: Bool = false
+    environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
+    clientKey: String = testClientKey,
+    countryCode: String = "NL",
+    amount: AmountDTO? = AmountDTO(currency: "EUR", value: 1000),
+    shopperLocale: String? = nil,
+    cardConfigurationDTO: CardConfigurationDTO? = nil,
+    applePayConfigurationDTO: ApplePayConfigurationDTO? = nil,
+    cashAppPayConfigurationDTO: CashAppPayConfigurationDTO? = nil,
+    twintConfigurationDTO: TwintConfigurationDTO? = nil,
+    threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO? = nil,
+    analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0"),
+    showPreselectedStoredPaymentMethod: Bool = true,
+    skipListWhenSinglePaymentMethod: Bool = false,
+    isRemoveStoredPaymentMethodEnabled: Bool = false
 ) -> DropInConfigurationDTO {
-  DropInConfigurationDTO(
-    environment: environment,
-    clientKey: clientKey,
-    countryCode: countryCode,
-    amount: amount,
-    shopperLocale: shopperLocale,
-    cardConfigurationDTO: cardConfigurationDTO,
-    applePayConfigurationDTO: applePayConfigurationDTO,
-    googlePayConfigurationDTO: nil,
-    cashAppPayConfigurationDTO: cashAppPayConfigurationDTO,
-    twintConfigurationDTO: twintConfigurationDTO,
-    threeDS2ConfigurationDTO: threeDS2ConfigurationDTO,
-    analyticsOptionsDTO: analyticsOptionsDTO,
-    showPreselectedStoredPaymentMethod: showPreselectedStoredPaymentMethod,
-    skipListWhenSinglePaymentMethod: skipListWhenSinglePaymentMethod,
-    isRemoveStoredPaymentMethodEnabled: isRemoveStoredPaymentMethodEnabled,
-    preselectedPaymentMethodTitle: nil,
-    paymentMethodNames: nil,
-    isPartialPaymentSupported: false
-  )
+    DropInConfigurationDTO(
+        environment: environment,
+        clientKey: clientKey,
+        countryCode: countryCode,
+        amount: amount,
+        shopperLocale: shopperLocale,
+        cardConfigurationDTO: cardConfigurationDTO,
+        applePayConfigurationDTO: applePayConfigurationDTO,
+        googlePayConfigurationDTO: nil,
+        cashAppPayConfigurationDTO: cashAppPayConfigurationDTO,
+        twintConfigurationDTO: twintConfigurationDTO,
+        threeDS2ConfigurationDTO: threeDS2ConfigurationDTO,
+        analyticsOptionsDTO: analyticsOptionsDTO,
+        showPreselectedStoredPaymentMethod: showPreselectedStoredPaymentMethod,
+        skipListWhenSinglePaymentMethod: skipListWhenSinglePaymentMethod,
+        isRemoveStoredPaymentMethodEnabled: isRemoveStoredPaymentMethodEnabled,
+        preselectedPaymentMethodTitle: nil,
+        paymentMethodNames: nil,
+        isPartialPaymentSupported: false
+    )
 }
 
 func createCardConfigurationDTO(
-  holderNameRequired: Bool = false,
-  addressMode: AddressMode = .none,
-  showStorePaymentField: Bool = false,
-  showCvcForStoredCard: Bool = true,
-  showCvc: Bool = true,
-  kcpFieldVisibility: FieldVisibility = .hide,
-  socialSecurityNumberFieldVisibility: FieldVisibility = .hide,
-  supportedCardTypes: [String?] = [],
-  installmentConfiguration: InstallmentConfigurationDTO? = nil
+    holderNameRequired: Bool = false,
+    addressMode: AddressMode = .none,
+    showStorePaymentField: Bool = false,
+    showCvcForStoredCard: Bool = true,
+    showCvc: Bool = true,
+    kcpFieldVisibility: FieldVisibility = .hide,
+    socialSecurityNumberFieldVisibility: FieldVisibility = .hide,
+    supportedCardTypes: [String?] = [],
+    installmentConfiguration: InstallmentConfigurationDTO? = nil
 ) -> CardConfigurationDTO {
-  CardConfigurationDTO(
-    holderNameRequired: holderNameRequired,
-    addressMode: addressMode,
-    showStorePaymentField: showStorePaymentField,
-    showCvcForStoredCard: showCvcForStoredCard,
-    showCvc: showCvc,
-    kcpFieldVisibility: kcpFieldVisibility,
-    socialSecurityNumberFieldVisibility: socialSecurityNumberFieldVisibility,
-    supportedCardTypes: supportedCardTypes,
-    installmentConfiguration: installmentConfiguration
-  )
+    CardConfigurationDTO(
+        holderNameRequired: holderNameRequired,
+        addressMode: addressMode,
+        showStorePaymentField: showStorePaymentField,
+        showCvcForStoredCard: showCvcForStoredCard,
+        showCvc: showCvc,
+        kcpFieldVisibility: kcpFieldVisibility,
+        socialSecurityNumberFieldVisibility: socialSecurityNumberFieldVisibility,
+        supportedCardTypes: supportedCardTypes,
+        installmentConfiguration: installmentConfiguration
+    )
 }
 
 func createBlikComponentConfigurationDTO(
-  environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
-  clientKey: String = testClientKey,
-  countryCode: String = "PL",
-  amount: AmountDTO? = AmountDTO(currency: "PLN", value: 1000),
-  shopperLocale: String? = nil,
-  analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0")
+    environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
+    clientKey: String = testClientKey,
+    countryCode: String = "PL",
+    amount: AmountDTO? = AmountDTO(currency: "PLN", value: 1000),
+    shopperLocale: String? = nil,
+    analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0")
 ) -> BlikComponentConfigurationDTO {
-  BlikComponentConfigurationDTO(
-    environment: environment,
-    clientKey: clientKey,
-    countryCode: countryCode,
-    amount: amount,
-    shopperLocale: shopperLocale,
-    analyticsOptionsDTO: analyticsOptionsDTO
-  )
+    BlikComponentConfigurationDTO(
+        environment: environment,
+        clientKey: clientKey,
+        countryCode: countryCode,
+        amount: amount,
+        shopperLocale: shopperLocale,
+        analyticsOptionsDTO: analyticsOptionsDTO
+    )
 }
 
 func createActionComponentConfigurationDTO(
-  environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
-  clientKey: String = testClientKey,
-  shopperLocale: String? = nil,
-  amount: AmountDTO? = nil,
-  analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0"),
-  threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO? = nil
+    environment: adyen_checkout.Environment = adyen_checkout.Environment.test,
+    clientKey: String = testClientKey,
+    shopperLocale: String? = nil,
+    amount: AmountDTO? = nil,
+    analyticsOptionsDTO: AnalyticsOptionsDTO = AnalyticsOptionsDTO(enabled: true, version: "1.0.0"),
+    threeDS2ConfigurationDTO: ThreeDS2ConfigurationDTO? = nil
 ) -> ActionComponentConfigurationDTO {
-  ActionComponentConfigurationDTO(
-    environment: environment,
-    clientKey: clientKey,
-    shopperLocale: shopperLocale,
-    amount: amount,
-    analyticsOptionsDTO: analyticsOptionsDTO,
-    threeDS2ConfigurationDTO: threeDS2ConfigurationDTO
-  )
+    ActionComponentConfigurationDTO(
+        environment: environment,
+        clientKey: clientKey,
+        shopperLocale: shopperLocale,
+        amount: amount,
+        analyticsOptionsDTO: analyticsOptionsDTO,
+        threeDS2ConfigurationDTO: threeDS2ConfigurationDTO
+    )
 }
