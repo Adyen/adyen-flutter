98c1c1c71c8bd47b2472fa124f9bd8968f4c1ff3
Robert Schulze Dieckhoff <13377878+Robert-SD@users.noreply.github.com>
2026-05-01 14:05:52 +0200

    Align method name with iOS

--- android/src/main/kotlin/com/adyen/checkout/flutter/dropIn/DropInPlatformApi.kt
+++ android/src/main/kotlin/com/adyen/checkout/flutter/dropIn/DropInPlatformApi.kt
@@ -109,7 +109,7 @@ internal class DropInPlatformApi(
                     dropInConfigurationDTO.isPartialPaymentSupported
                 )
             val filteredPaymentMethods =
-                hideStoredPaymentMethodsIfNeeded(
+                applyStoredPaymentMethodsVisibility(
                     paymentMethodsWithoutGiftCards,
                     dropInConfigurationDTO.showStoredPaymentMethods
                 )
