1c4dce564d49bbc937c8acf140334fe200012ba1
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2026-08-06 18:18:49 +0200

    test: Add Flutter Apple Pay E2E support

--- example/lib/screens/component/apple_pay/apple_pay_session_component_screen.dart
+++ example/lib/screens/component/apple_pay/apple_pay_session_component_screen.dart
@@ -57,17 +57,21 @@ class ApplePaySessionComponentScreen extends StatelessWidget {
                 "Session flow",
               ),
               const SizedBox(height: 8),
-              AdyenApplePayComponent(
-                configuration: applePayComponentConfiguration,
-                paymentMethod: paymentMethod,
-                checkout: sessionCheckout,
-                loadingIndicator: const CircularProgressIndicator(),
-                width: 200,
-                height: 48,
-                onPaymentResult: (paymentResult) {
-                  Navigator.pop(context);
-                  DialogBuilder.showPaymentResultDialog(paymentResult, context);
-                },
+              Semantics(
+                identifier: "e2e-apple-pay-button",
+                child: AdyenApplePayComponent(
+                  configuration: applePayComponentConfiguration,
+                  paymentMethod: paymentMethod,
+                  checkout: sessionCheckout,
+                  loadingIndicator: const CircularProgressIndicator(),
+                  width: 200,
+                  height: 48,
+                  onPaymentResult: (paymentResult) {
+                    Navigator.pop(context);
+                    DialogBuilder.showPaymentResultDialog(
+                        paymentResult, context);
+                  },
+                ),
               ),
             ],
           );
